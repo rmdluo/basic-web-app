64fb70e7cb5ad1dc3b51e33198933f8024b8a834
Raymond Luo <rmdluo@gmail.com>
2024-03-14 16:10:16 -0400

    added max query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -29,5 +29,15 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("Which of the following numbers is the largest: 44, 32, 18?") && query.split(" ").length >= 11) {
+    let num1 : number = Number(query.split(" ")[8]);
+    let num2 : number = Number(query.split(" ")[9]);
+    let num3 : number = Number(query.split(" ")[10].slice(0, -1));
+    
+    return (
+      (Math.max(num1, num2, num3)).toString()
+    );
+  }
+
   return "";
 }
